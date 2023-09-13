39dea47e7423cf3495a1a0f1afa153f2abc9491b
RishiPandey9 <144772531+RishiPandey9@users.noreply.github.com>
2023-09-13 13:12:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main 
+{
+ int i;
+ private Main ()
+  {
+    i=5;
+    System.out.println("Constructor is called");
+
+  }
+  public static void main(String args[])
+  {
+    Main obj = new Main ();
+    System.out.println("value of i:" + obj.i);
+  }   
+}
